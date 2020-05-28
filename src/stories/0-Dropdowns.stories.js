f50f165c0d14e90a770f4ed494c8a38716240ba5
trinitythompson <trinity@shyftsolutions.io>
2020-05-28 14:25:20 -0500

    Added folder and files for a dropdown component

---
+++ src/stories/0-Dropdowns.stories.js
@@ -0,0 +1,8 @@
+import _SimpleSelect from 'components/dropdown/SimpleSelect';
+import React from 'react';
+
+export default {
+    title: 'Dropdowns',
+};
+
+export const SimpleSelect = () => <_SimpleSelect />;
