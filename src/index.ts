09c9c1d7919a393d2d6af66f7c2b41b663f2959c
Alex Olsen <alex@Shyfts-MacBook-Pro.local>
2020-06-10 14:44:57 -0500

    adding missing components

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import BaseWxViewer from './components/viewers/BaseWxViewer';
 import GroupedButtons from './components/buttons/GroupedButtons';
 import SimpleSelect from './components/dropdown/SimpleSelect';
 import ProductMenu from './components/products/ProductMenu';
-//import ProductList from './products/ProductList'
+import ProductSelector from './components/products/ProductSelector';
 import BackButton from './components/time/BackButton';
 import ForwardButton from './components/time/ForwardButton';
 import Slider from './components/time/Slider';
 import StartStopButton from './components/time/StartStopButton';
 import TimeControl from './components/time/TimeControl';
 import ShyftWx from './components/root/ShyftWx';
+import ShyftModel from './components/models/ShyftModel';
+import RegionSelector from './components/regions/RegionSelector';
 import theme from './theme';
 import * as apis from './apis';
 
@@ -17,7 +19,9 @@ export {
     GroupedButtons,
     SimpleSelect,
     ProductMenu,
-    // ProductList,
+    ProductSelector,
+    RegionSelector,
+    ShyftModel,
     BackButton,
     ForwardButton,
     Slider,
