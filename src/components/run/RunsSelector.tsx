2070d741fdf660d8ad7282dd7ad17b63b3a3a5f5
trinitythompson <trinity@shyftsolutions.io>
2020-06-15 17:20:43 -0500

    changed name of a function

--- src/components/run/RunsSelector.tsx
+++ src/components/run/RunsSelector.tsx
@@ -3,27 +3,9 @@ import { Grid, Typography } from '@material-ui/core';
 import GroupedButtons from '../buttons/GroupedButtons';
 import React from 'react';
 
-const toDates = (options: any) => {
-    const dates: string[] = [];
-
-    options.map((option) => {
-        const epoch: number = option * 1000;
-        const date: Date = new Date(epoch);
-        let time: string;
-        if (date.getUTCHours() === 0) {
-            time = `${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDay()}T 00:${date.getUTCMinutes()}Z`;
-        } else if (date.getUTCMinutes() === 0) {
-            time = `${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDay()}T ${date.getUTCHours()}:00Z`;
-        } else {
-            time = `${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDay()}T ${date.getUTCHours()}:${date.getUTCMinutes()}Z`;
-        }
-        dates.push(time);
-    });
-
-    return dates;
-};
 /**
  * Creates a Material UI Grid Item for the Region button group
+ *
  *
  * @param Props: { options: string[]}
  */
