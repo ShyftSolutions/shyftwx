57c08d0fd59091a90ad2244e6aa0dc35be58a2e2
Alex Olsen <alex@Shyfts-MacBook-Pro.local>
2020-08-07 14:55:49 -0500

    working transformation of data

--- src/components/car-route/Route.tsx
+++ src/components/car-route/Route.tsx
@@ -11,32 +11,69 @@ export const Route: React.FC<RouteProps> = ({ legs }) => {
     // const [endCoords, setEndCoords] = React.useState<number[]>([]);
 
     const getColor = (leg) => {
-        // the standards to compare against
-        const isGreaterThan = leg.threshold.greaterThan;
-        const firstThreshold = leg.threshold.threshold[0];
-        const secondThreshold = leg.threshold.threshold[1];
 
-        // the value to compare
-        const value = leg.featureValues.temp.value;
-        const name = leg.featureValues.temp.name;
+        console.log(leg);
 
-        if (isGreaterThan) {
-            if (value > secondThreshold) {
-                return 'red';
-            } else if (value > firstThreshold) {
-                return 'yellow';
+        // get all 3 features we care about (wind, temp...)
+
+        // for every feature, seee if its values violate the thresholds we set
+
+        // default
+        let overallColor = 'green'
+        const thresholds = leg.threshold;
+
+        Object.keys(leg.featureValues).forEach(featureValueKey => {
+            const featureValue = leg.featureValues[featureValueKey]
+            // i.e. 32.0 for "Temperature"
+            const legFeatureValue = featureValue.value
+
+            const isGreaterThan = thresholds[featureValue.name].greaterThan;
+            const firstThreshold = thresholds[featureValue.name].threshold[0];
+            const secondThreshold = thresholds[featureValue.name].threshold[1];
+
+            if (isGreaterThan) {
+                if (legFeatureValue > secondThreshold) {
+                    overallColor = 'red';
+                } else if (legFeatureValue > firstThreshold && overallColor != 'red') {
+                    overallColor = 'yellow'
+                }
             } else {
-                return 'green';
+
             }
-        } else {
-            if (value < secondThreshold) {
-                return 'red';
-            } else if (value < firstThreshold) {
-                return 'yellow';
-            } else {
-                return 'green';
-            }
-        }
+        });
+
+        return overallColor;
+
+
+
+
+
+        // // the standards to compare against
+        // const isGreaterThan = leg.threshold.greaterThan;
+        // const firstThreshold = leg.threshold.threshold[0];
+        // const secondThreshold = leg.threshold.threshold[1];
+
+        // // the value to compare
+        // const value = leg.featureValues.temp.value;
+        // const name = leg.featureValues.temp.name;
+
+        // if (isGreaterThan) {
+        //     if (value > secondThreshold) {
+        //         return 'red';
+        //     } else if (value > firstThreshold) {
+        //         return 'yellow';
+        //     } else {
+        //         return 'green';
+        //     }
+        // } else {
+        //     if (value < secondThreshold) {
+        //         return 'red';
+        //     } else if (value < firstThreshold) {
+        //         return 'yellow';
+        //     } else {
+        //         return 'green';
+        //     }
+        // }
     };
 
     return (
