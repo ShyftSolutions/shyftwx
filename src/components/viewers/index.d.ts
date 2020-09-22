cda52309f1934c724f03d62e87bf46c93cd5fd8a
Michael Ferguson <mike@shyftsolutions.io>
2020-09-22 10:15:24 -0500

    - Started code for custom marker/image layers - Updated the BaseWxViewer story

--- src/components/viewers/index.d.ts
+++ src/components/viewers/index.d.ts
@@ -4,4 +4,38 @@
 
 declare type ImageViewerProps = {
     image: string;
-}
+};
+
+declare type WxViewerLayer = {
+    type: 'image' | 'metar';
+    coordinates: import('leaflet').LatLngExpression;
+};
+
+declare type MetarPayload = {
+    altimeter: number;
+    automation: string;
+    ceiling: number;
+    cloud_coverages: number[];
+    cloud_layers: number[];
+    dewpoint: number;
+    elevation: number;
+    present_wx: number;
+    raw_ob: string;
+    report_id: string;
+    report_type: string;
+    sky_cover: number;
+    temperature: number;
+    valid_time: Date;
+    visibility: number;
+    wind_direction: number;
+    wind_gust: number;
+    wind_speed: number;
+};
+
+declare type WxViewerLayerMetar = WxViewerLayer & { type: 'metar'; payload: MetarPayload };
+
+declare type BaseWxViewerProps = {
+    layers: WxViewerLayer[];
+    neBounds: import('leaflet').LatLngExpression;
+    swBounds: import('leaflet').LatLngExpression;
+};
--- src/stories/0-Viewer.stories.js
+++ src/stories/0-Viewer.stories.js
@@ -11,30 +11,29 @@ import { faPercent, faTint, faWind } from '@fortawesome/free-solid-svg-icons';
 
 export default {
     title: 'Viewer',
-    decorators: [withKnobs],
+    decorators: [withKnobs]
 };
 
-
 const defaultViewerKnobs = {
-
     viewerKnobs: {
         swBounds: [23.81, -65.69],
         neBounds: [49.38, -129.17]
     },
 
     modelButtonKnobs: {
-        label: "Model",
+        label: 'Model',
         buttonLabels: [
-        {
-            name: "TQI Model"
-        },
-        {
-            name: "GFS"
-        },
-        {
-            name: "HRRR"
-        }
-    ]},
+            {
+                name: 'TQI Model'
+            },
+            {
+                name: 'GFS'
+            },
+            {
+                name: 'HRRR'
+            }
+        ]
+    },
 
     sliderKnobs: {
         maxValue: 12,
@@ -42,86 +41,86 @@ const defaultViewerKnobs = {
         marks: [
             {
                 value: 0,
-                label: '0H',
+                label: '0H'
             },
             {
                 value: 1,
-                label: '1H',
+                label: '1H'
             },
             {
                 value: 2,
-                label: '2H',
+                label: '2H'
             },
             {
                 value: 3,
-                label: '3H',
+                label: '3H'
             },
             {
                 value: 4,
-                label: '4H',
+                label: '4H'
             },
             {
                 value: 5,
-                label: '5H',
+                label: '5H'
             },
             {
                 value: 6,
-                label: '6H',
+                label: '6H'
             },
             {
                 value: 7,
-                label: '7H',
+                label: '7H'
             },
             {
                 value: 8,
-                label: '8H',
+                label: '8H'
             },
             {
                 value: 9,
-                label: '9H',
+                label: '9H'
             },
             {
                 value: 10,
-                label: '10H',
+                label: '10H'
             },
             {
                 value: 11,
-                label: '11H',
+                label: '11H'
             },
             {
                 value: 12,
-                label: '12H',
+                label: '12H'
             }
-        ],
-
+        ]
     },
 
     regionButtonKnobs: {
-        label: "Region",
+        label: 'Region',
         buttonLabels: [
-        {
-            name: "TQI Model"
-        },
-        {
-            name: "CONUS"
-        },
-        {
-            name: "Southeast"
-        }
-    ]},
+            {
+                name: 'TQI Model'
+            },
+            {
+                name: 'CONUS'
+            },
+            {
+                name: 'Southeast'
+            }
+        ]
+    },
 
     selectKnobs: [
         {
-            name: "2020-05-27T 12:00:00Z"
+            name: '2020-05-27T 12:00:00Z'
         },
         {
-            name: "2020-05-27T 06:00:00Z"
+            name: '2020-05-27T 06:00:00Z'
         },
         {
-            name: "2020-05-27T 11:00:00Z"
+            name: '2020-05-27T 11:00:00Z'
         },
         {
-            name: "2020-05-27T 02:00:00Z"
+            name: '2020-05-27T 02:00:00Z'
         }
     ],
 
@@ -129,41 +128,76 @@ const defaultViewerKnobs = {
         {
             name: 'Surface',
             open: true,
-            products: [{
-                name: 'Wind and Temperature',
-                icon: faWind,
-            }, {
-                name: 'Relative Humidity',
-                icon: faPercent,
-            }]
+            products: [
+                {
+                    name: 'Wind and Temperature',
+                    icon: faWind
+                },
+                {
+                    name: 'Relative Humidity',
+                    icon: faPercent
+                }
+            ]
         },
         {
             name: '850MB',
             open: false,
-            products: [{
-                name: 'Relative Humidity',
-                icon: faTint,
-            },]
+            products: [
+                {
+                    name: 'Relative Humidity',
+                    icon: faTint
+                }
+            ]
         },
         {
             name: '700MB',
             open: false,
-            products: [{
-                name: 'Wind and Temperature',
-                icon: faWind,
-            },]
+            products: [
+                {
+                    name: 'Wind and Temperature',
+                    icon: faWind
+                }
+            ]
         }
     ]
-
 };
 
 export const BaseWxViewer = () => {
     return (
         <MuiThemeProvider theme={theme}>
-            <_BaseWxViewer neBounds={object('neBounds', [49.38, -129.17])} swBounds={object('swBounds', [23.81, -65.69])}/>
+            <_BaseWxViewer
+                neBounds={object('neBounds', [49.38, -129.17])}
+                swBounds={object('swBounds', [23.81, -65.69])}
+                layers={[
+                    {
+                        type: 'metar',
+                        coordinates: [41.12, -95.92],
+                        payload: {
+                            altimeter: 102300.0,
+                            automation: 'AO2A',
+                            ceiling: null,
+                            cloud_coverages: [],
+                            cloud_layers: [],
+                            dewpoint: 286.45,
+                            elevation: 319,
+                            present_wx: null,
+                            raw_ob: 'KOFF 211456Z AUTO 19008KT 10SM CLR 18/13 A3021 RMK AO2 SLP232 T01780133 50004 $',
+                            report_id: 'KOFF',
+                            report_type: 'METAR',
+                            sky_cover: 0,
+                            temperature: 290.95,
+                            valid_time: '2020-09-21T14:56:00Z',
+                            visibility: 16090,
+                            wind_direction: 190,
+                            wind_gust: null,
+                            wind_speed: 4.1
+                        }
+                    }
+                ]}
+            />
         </MuiThemeProvider>
     );
-}
+};
 
 export const ImageViewer = () => {
     return (
@@ -171,7 +205,7 @@ export const ImageViewer = () => {
             <_ImageViewer image="https://s3-us-east-2.amazonaws.com/wxchange-images/5b4daa25-3d9f-4f83-ade4-ee6976b259e1/TQIConus/images/AAAD00_1591524000_TQI_Temperature_2m_900.PNG" />
         </MuiThemeProvider>
     );
-}
+};
 export const WxViewer = () => <_WxViewer />;
 
 export const StaticWxViewer = () => <_StaticWxViewer />;
