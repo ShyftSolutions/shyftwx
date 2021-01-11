63ad9b7f74e59047a489f2d5931c949818c9d350
leaholson <leah@shyftsolutions.io>
2021-01-11 11:36:30 -0600

    Added to index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import ProductMenu from './components/products/ProductMenu';
 import SideMenu from './components/products/SideMenu';
 import RegionSelector from './components/regions/RegionSelector';
 import LandingPage from './components/root/LandingPage';
+import ShyftWxStatic from './components/root/ShyftWxStatic';
 import ShyftWx from './components/root/ShyftWx';
 import RunsSelector from './components/run/RunsSelector';
 import BackButton from './components/time/BackButton';
@@ -28,6 +29,7 @@ export {
     RunsSelector,
     Slider,
     ShyftWx,
+    ShyftWxStatic,
     StartStopButton,
     TimeControl,
     theme,
